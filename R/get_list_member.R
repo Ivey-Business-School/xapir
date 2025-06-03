ae8c476020e14622287e826e4de6d80450569999
Michael Gao <mgao244@uwo.ca>
2025-06-03 18:32:57 -0400

    Adjusted get_list_member() to return more user information

--- R/get_list_member.R
+++ R/get_list_member.R
@@ -4,10 +4,12 @@
 #' Returns a list of Users that are members of a List by the provided List ID via the [get list 
 #' members](https://docs.x.com/x-api/users/returns-user-objects-that-are-members-of-a-list-by-the-provided-list-id).
 #'
-#' @importFrom httr2 oauth_client oauth_flow_auth_code request req_auth_bearer_token req_perform resp_body_json
+#' @importFrom httr2 request req_auth_bearer_token req_url_query req_perform resp_body_json
+#' @importFrom stringr str_c
 #' @param list_id A string representing the unique ID of the list.
 #' @template bearer_token 
-#' @return A tibble with user details (ID, name, username) of list members, or NULL if none found.
+#' @template user_fields
+#' @return A list containing the API response.
 #' @examples
 #' \dontrun{
 #' list_members <- get_list_members(list_ID)
@@ -15,24 +17,25 @@
 #' @export
 get_list_member <- function(
     list_id, 
-    bearer_token = Sys.getenv("X_BEARER_TOKEN")
+    bearer_token = Sys.getenv("X_BEARER_TOKEN"),
+    user_fields      =
+      c("created_at", "description", "protected", "entities", "location",
+        "profile_image_url", "public_metrics", "verified", "verified_type")
 ) {
 
-  # Make request to list members endpoint
   url <- paste0("https://api.twitter.com/2/lists/", list_id, "/members")
+  user_fields_str <- str_c(user_fields, collapse = ",")
 
-  members_req <- request(url) |>
+  resp <- request(url) |>
+    req_url_query(user.fields = user_fields_str) |>
     req_auth_bearer_token(bearer_token) |>
     req_perform()
 
-  members_data <- resp_body_json(members_req)
+  json <- resp_body_json(resp)
 
-  # Extract member data
-  if (!is.null(members_data$data)) {
-    df <- as.data.frame(do.call(rbind, lapply(members_data$data, as.data.frame)))
-    return(df)
-  } else {
-    message("No members found in this list.")
-    return(NULL)
-  }
+  return(list(
+    list(
+      includes = list(users = json$data %||% list()),
+      meta = json$meta %||% list()
+  )))
 }
