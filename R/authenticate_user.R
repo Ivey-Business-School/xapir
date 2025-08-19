7c4f31f27ebcacc6c671ed1f66aa9b7326efcc59
Michael Gao <mgao244@uwo.ca>
2025-08-18 23:46:24 -0400

    Uncomment out part of function

--- R/authenticate_user.R
+++ R/authenticate_user.R
@@ -12,35 +12,35 @@ authenticate_user <- function(
   client_id = Sys.getenv("X_CLIENT_ID")
 ) {
   
-  # # Check in-memory token
-  # if (!is.null(.x_env$token)) {
-  #   token <- .x_env$token
+  # Check in-memory token
+  if (!is.null(.x_env$token)) {
+    token <- .x_env$token
 
-  #   if (!is.null(token$expires_at) && Sys.time() < token$expires_at) {
-  #     return(token)
-  #   } 
+    if (!is.null(token$expires_at) && Sys.time() < token$expires_at) {
+      return(token)
+    } 
     
-  #   # Try to refresh token if possible
-  #   if (!is.null(token$refresh_token)) {
-  #     message("Access token expired. Attempting refresh...")
+    # Try to refresh token if possible
+    if (!is.null(token$refresh_token)) {
+      message("Access token expired. Attempting refresh...")
 
-  #     client <- oauth_client(
-  #       id = client_id,
-  #       token_url = "https://api.twitter.com/2/oauth2/token"
-  #     )
+      client <- oauth_client(
+        id = client_id,
+        token_url = "https://api.twitter.com/2/oauth2/token"
+      )
 
-  #     refreshed_token <- oauth_flow_refresh(
-  #       client = client,
-  #       refresh_token = token$refresh_token,
-  #       scope = token$scope
-  #     )
+      refreshed_token <- oauth_flow_refresh(
+        client = client,
+        refresh_token = token$refresh_token,
+        scope = token$scope
+      )
 
-  #     .x_env$token <- refreshed_token
-  #     return(refreshed_token)
-  #   }
+      .x_env$token <- refreshed_token
+      return(refreshed_token)
+    }
     
-  #   message("Token expired and no refresh token. Re-authenticating...")
-  # }
+    message("Token expired and no refresh token. Re-authenticating...")
+  }
 
   # Re-authenticate
   client <- oauth_client(
